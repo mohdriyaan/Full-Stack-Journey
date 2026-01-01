04b9a0d3f5df6fc1c639d3abac67e93578974e84
mohdriyaan <mohammedrayaan1@gmail.com>
2026-01-01 20:25:21 +0000

    files commited

---
+++ 13.Databases/Mongoose-Express/index.js
@@ -0,0 +1,66 @@
+const express =  require("express")
+const app = express()
+const mongoose = require("mongoose")
+const Product = require("./models/product")
+const methodOverride = require("method-override")
+
+app.set("view engine","ejs")
+app.set("views","views")
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride("_method"))
+
+const categories = ["fruit","vegetable","dairy"]
+
+mongoose.connect("mongodb://localhost:27017/farmStand")
+    .then(()=>{
+        console.log("Mongo Connection Established")
+    })
+    .catch((err)=>{
+        console.log("Mongo Connection Error :", err.errors)
+    })
+
+app.get("/products",async (req,res)=>{
+    const products = await Product.find({})
+    res.render("products/index",{products})
+})
+
+app.get("/products/new",(req,res)=>{
+    res.render("products/new",{categories})
+})
+
+app.post("/products", async (req,res)=>{
+    const newProduct = new Product(req.body)
+    await newProduct.save()
+    res.redirect(`/products/${newProduct.id}`)
+})
+
+app.get("/products/:id",async(req,res)=>{
+    const {id} = req.params
+    const product = await Product.findById(id)
+    res.render("products/show",{product})
+})
+
+app.get("/products/:id/edit",async (req,res)=>{
+    const {id} = req.params
+    const product = await Product.findById(id)
+    res.render("products/edit",{product,categories})
+})
+
+app.put("/products/:id", async(req,res)=>{
+    const {id} = req.params
+    const product = await Product.findByIdAndUpdate(id,req.body,{runValidators:true})
+    res.redirect(`/products/${product._id}`)
+})
+
+app.delete("/products/:id", async(req,res)=>{
+    const {id} = req.params
+    const product = await Product.findByIdAndDelete(id)
+    res.redirect("/products")
+})
+
+
+app.listen(3000,()=>{
+    console.log("Server Listening at PORT 3000")
+})
+
+
---
+++ 13.Databases/Mongoose-Express/seeds.js
@@ -0,0 +1,64 @@
+const Product = require("./models/product")
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/farmStand")
+    .then(()=>{
+        console.log("Mongo Connection Established")
+    })
+    .catch((err)=>{
+        console.log(err.errors)
+    })
+
+// inserts only one product
+
+// const p = new Product({
+//     name : "Ruby GrapeFruit",
+//     price : "1.99",
+//     category: "fruit"
+// })    
+
+// p.save()
+//     .then(()=>{
+//         console.log(p)
+//     })
+//     .catch((err)=>{
+//         console.log(err)
+//     })
+
+// insert many products
+const seedProducts = [
+    {
+        name:"Fairy Eggplant",
+        price:1.00,
+        category:"vegetable"
+    },
+    {
+        name : "Organic Goddess Melon",
+        price:4.99,
+        category:"fruit"
+    },
+    {
+        name:"Organic Mini Seedless Watermelon",
+        price:3.99,
+        category:"fruit"
+    },
+    {
+        name:"Organic Celery",
+        price:1.50,
+        category:"vegetable"
+    },
+    {
+        name:"Chocolate Whole Milk",
+        price:2.69,
+        category:"Dairy"
+    }
+]
+
+Product.insertMany(seedProducts)
+    .then((res)=>{
+        console.log(res)
+    })
+    .catch((err)=>{
+        console.log(err.errors)
+    })
+    
