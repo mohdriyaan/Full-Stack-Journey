33edcb34566c4b192d6295d6c345460bafbdbf82
mohdriyaan <mohammedrayaan1@gmail.com>
2026-01-07 10:37:50 +0000

    files commited

--- 13.Databases/Mongoose-Express/index.js
+++ 13.Databases/Mongoose-Express/index.js
@@ -20,8 +20,14 @@ mongoose.connect("mongodb://localhost:27017/farmStand")
     })
 
 app.get("/products",async (req,res)=>{
-    const products = await Product.find({})
-    res.render("products/index",{products})
+    const {category} = req.query
+    if(category){
+        const products = await Product.find({category})
+        res.render("products/index",{products,category})
+    }else{
+        const products = await Product.find({})
+        res.render("products/index",{products,category:"All"})
+    }
 })
 
 app.get("/products/new",(req,res)=>{
